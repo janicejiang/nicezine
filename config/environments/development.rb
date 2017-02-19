3043c5c19b7dfc50e3b97158450584aecb241c74
janice <janicejiang.d@gmail.com>
2017-02-19 23:22:30 +0800

    fix seng_cloud

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # config.action_mailer.delivery_method = :letter_opener
-  ＋  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: "smtpcloud.sohu.com",
     port: 25,
