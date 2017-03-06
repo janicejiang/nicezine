35010ab996943d4056ef209289af35cc501c3d07
janice <janicejiang.d@gmail.com>
2017-03-06 13:23:04 +0800

    navbar collapse

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap/alert
-//= require bootstrap/dropdown
-//= require bootstrap/carousel
-//= require bootstrap/modal
 //= require social-share-button
 //= require bootstrap-sprockets
 //= require_tree .
